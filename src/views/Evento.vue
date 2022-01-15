50240aa398528a5d44d2005a457aae838a38bc36
mcelam00 <mcelam00@estudiantes.unileon.es>
2022-01-15 01:45:16 +0100

    calendario eventos modificado

--- src/views/Evento.vue
+++ src/views/Evento.vue
@@ -2,7 +2,7 @@
   <v-app class="pt-5">
     <div class="d-flex justify-center">
       <v-card
-        height="990"
+        height="750"
         width="1800"
         class="rounded-lg"
         style="
@@ -54,14 +54,16 @@
             <h3 class="pt-3">(153 valoraciones)</h3>
           </v-row>
         </div>
-        <v-row class="mb-6" no-gutters>
-          <v-col md="4" class="pl-8">
-            <div class="ml-16 mt-8" style="width: 800px">
-              <v-row>
-                <v-col v-for="(p, i) in descripciones" :key="i" cols="5">
-                  <v-card
+
+
+
+           <div class="pl-15 mt-12">
+                <v-row>
+                 
+                  <v-card v-for="(p, i) in descripciones" :key="i" 
                     class="ma-3"
-                    height="160"
+                    height="150"
+                    width="400"
                     style="
                       background: #dedede;
                       box-shadow: 5px 5px 10px #b4b4b4, -5px -5px 10px #ffffff;
@@ -79,35 +81,179 @@
                       {{ p.desc }}</v-card-subtitle
                     >
                   </v-card>
-                </v-col></v-row
-              >
-            </div>
-          </v-col>
-          <v-col md="4" offset-md="3">
-            <div
-              class="pb-2 rounded-lg"
-              style="
-                background: #dedede;
-                box-shadow: 5px 5px 10px #b4b4b4, -5px -5px 10px #ffffff;
-              "
-            >
-              <h2 class="pl-4 pt-4">
+                </v-row>
+            
+                            </div>
+
+
+
+
+      </v-card>
+
+    </div>
+    
+ 
+    <div class="d-flex justify-center">
+
+    <v-card
+        height="1000"
+        width="1800"
+        class="rounded-lg mt-6"
+        style="
+          background: #dedede;
+          box-shadow: 5px 5px 10px #b4b4b4, -5px -5px 10px #ffffff;
+        "
+      > <h2 class="pl-9 pt-4 py-3">
                 <v-icon large>mdi-calendar-month-outline</v-icon>
                 Hacer reserva
               </h2>
-              <div class="ma-5">
-                <v-date-picker
-                  full-width
-                  elevation="1"
-                  class="pb-1"
-                  v-model="picker"
-                ></v-date-picker>
-              </div>
-            </div>
-          </v-col>
-        </v-row>
-      </v-card>
+
+        
+
+
+
+       <div class="pa-10 pt-4 rounded-lg">
+     
+  <v-row class="fill-height">
+    <v-col>
+      <v-sheet height="64">
+        <v-toolbar
+          flat
+        >
+          <v-btn
+            outlined
+            class="mr-4"
+            color="grey darken-2"
+            @click="setToday"
+          >
+            Today
+          </v-btn>
+          <v-btn
+            fab
+            text
+            small
+            color="grey darken-2"
+            @click="prev"
+          >
+            <v-icon small>
+              mdi-chevron-left
+            </v-icon>
+          </v-btn>
+          <v-btn
+            fab
+            text
+            small
+            color="grey darken-2"
+            @click="next"
+          >
+            <v-icon small>
+              mdi-chevron-right
+            </v-icon>
+          </v-btn>
+          <v-toolbar-title v-if="$refs.calendar">
+            {{ $refs.calendar.title }}
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-menu
+            bottom
+            right
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                outlined
+                color="grey darken-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <span>{{ typeToLabel[type] }}</span>
+                <v-icon right>
+                  mdi-menu-down
+                </v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="type = 'day'">
+                <v-list-item-title>Day</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'week'">
+                <v-list-item-title>Week</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'month'">
+                <v-list-item-title>Month</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = '4day'">
+                <v-list-item-title>4 days</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar>
+      </v-sheet>
+      <v-sheet height="800">
+        <v-calendar
+          ref="calendar"
+          v-model="focus"
+          color="primary"
+          :events="events"
+          :event-color="getEventColor"
+          :type="type"
+          @click:event="showEvent"
+          @click:more="viewDay"
+          @click:date="viewDay"
+          @change="updateRange"
+        ></v-calendar>
+        <v-menu
+          v-model="selectedOpen"
+          :close-on-content-click="false"
+          :activator="selectedElement"
+          offset-x
+        >
+          <v-card
+            color="grey lighten-4"
+            min-width="350px"
+            flat
+          >
+            <v-toolbar
+              :color="selectedEvent.color"
+              dark
+            >
+              <v-btn icon>
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
+              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </v-toolbar>
+            <v-card-text>
+              <span v-html="selectedEvent.details"></span>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                text
+                color="secondary"
+                @click="selectedOpen = false"
+              >
+                Cancel
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
+      </v-sheet>
+    </v-col>
+  </v-row>
+
+                </div>
+
+    </v-card>
+
+
     </div>
+
+
     <div class="my-5 ml-16">
       <h2>Opiniones</h2>
     </div>
@@ -136,9 +282,26 @@
 export default {
   name: "Evento",
 
-  mounted() {},
+
 
   data: () => ({
+
+focus: '',
+      type: 'month',
+      typeToLabel: {
+        month: 'Month',
+        week: 'Week',
+        day: 'Day',
+        '4day': '4 Days',
+      },
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false,
+      events: [],
+      colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
+      names: ['Naturaleza en directo', 'Descubriendo Arzuaga', 'Sensaciones enológicas', 'Taller de cata a cargo de nuestro equipo de sumillería'],
+ 
+
     picker: null,
 
     srcs: {
@@ -217,6 +380,81 @@ export default {
     print: function () {
       console.log(this.selector);
     },
+    viewDay ({ date }) {
+        this.focus = date
+        this.type = 'day'
+      },
+      getEventColor (event) {
+        return event.color
+      },
+      setToday () {
+        this.focus = ''
+      },
+      prev () {
+        this.$refs.calendar.prev()
+      },
+      next () {
+        this.$refs.calendar.next()
+      },
+      showEvent ({ nativeEvent, event }) {
+        const open = () => {
+          this.selectedEvent = event
+          this.selectedElement = nativeEvent.target
+          requestAnimationFrame(() => requestAnimationFrame(() => this.selectedOpen = true))
+        }
+
+        if (this.selectedOpen) {
+          this.selectedOpen = false
+          requestAnimationFrame(() => requestAnimationFrame(() => open()))
+        } else {
+          open()
+        }
+
+        nativeEvent.stopPropagation()
+      },
+      updateRange ({ start, end }) {
+        const events = []
+
+        const min = new Date(`${start.date}T00:00:00`)
+        const max = new Date(`${end.date}T23:59:59`)
+        const days = (max.getTime() - min.getTime()) / 86400000
+        const eventCount = this.rnd(days, days + 20)
+
+
+        
+
+        for (let i = 0; i < eventCount; i++) {
+          let random = this.rnd(0, this.names.length - 1);
+          const allDay = this.rnd(0, 3) === 0
+          const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+          const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+          const second = new Date(first.getTime() + secondTimestamp)
+
+          events.push({
+            /*name: this.names[this.rnd(0, this.names.length - 1)],
+            start: first,
+            end: second,
+            color: this.colors[this.rnd(0, this.colors.length - 1)],
+            timed: !allDay,*/
+
+           
+            name: this.names[random],
+            start: first,
+            end: second,
+            color: this.colors[random],
+            timed: !allDay,
+          })
+        }
+
+        this.events = events
+      },
+      rnd (a, b) {
+        return Math.floor((b - a + 1) * Math.random()) + a
+      },
   },
+     mounted () {
+      this.$refs.calendar.checkChange()
+    },
 };
 </script>
